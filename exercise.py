96f680f4a43526081cbbc15a020c4204cbdb8d13
Hassen <hmyws187404@gmail.com>
2024-03-05 23:53:47 +0300

    last commit of the day

--- exercise.py
+++ exercise.py
@@ -1,3 +1,5 @@
+from string import Template
+import datetime
 base = {1,2,3,4,5,6,7,8,9,10}
 square_of_base = { i ** 2 for i in base }
 print(square_of_base)
@@ -16,3 +18,13 @@ list2 = merged_string.split(", ")
 print(list2, list2)
 list3= list1+list2
 print(list3)
+name = 'smile'
+templ= Template('hello $name')
+print(templ.substitute(name=name))
+Time = datetime.time(1,30,45,56)
+print('the time is ', Time)
+my_birthday = datetime.date(2000, 1, 1)
+print(my_birthday)
+print('I was born on ', my_birthday.month, ' in ', my_birthday.year, ' on ', my_birthday.day)
+import re
+
